85ed3602f46eab9fdc8b69c0c7fdf04a7839f817
Garrett Cornwell <Garrett Cornwell>
2016-04-18 16:26:21 -0600

    alu and rom complete

--- FINAL_PROJECT/8-Bit_Microcomputer_Design/alu.vhd
+++ FINAL_PROJECT/8-Bit_Microcomputer_Design/alu.vhd
@@ -10,6 +10,7 @@
 ----------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all; 
+use IEEE.numeric_std.all; 
 
 entity alu is
 	port   (A			: in std_logic_vector (7 downto 0);
@@ -27,5 +28,71 @@ architecture alu_arch of alu is
   --signal  alu_NZVC : std_logic_vector (3 downto 0);
 
   begin
-      
+  
+	ALU_PROCESS : process (A, B, ALU_Sel)
+    
+		variable Sum_uns : unsigned (8 downto 0);
+		
+		begin
+			
+			if (ALU_Sel = "000") then -- ADDITION
+			
+				-- Sum
+				Sum_uns := unsigned('0' & A) + unsigned('0' & B);
+				Result <= std_logic_vector(Sum_uns(7 downto 0));
+				
+				-- NZVC Flags
+				
+					-- Negative Flag (N)
+					NZVC(3) <= Sum_uns(7); -- Negative Flag
+					
+					-- Zero Flag (Z)
+					if (Sum_uns(7 downto 0) = x"00") then
+						NZVC(2) <= '1';
+					else
+						NZVC(2) <= '0';
+					end if;
+					
+					-- Two's Complement Overflow Flag (V)
+					if ((A(7) = '0' and B(7) = '0' and Sum_uns(7) = '1') or
+						(A(7) = '1' and B(7) = '1' and Sum_uns(7) = '0')) then
+						NZVC(1) <= '1';
+					else
+						NZVC(1) <= '0';
+					end if;
+					
+					-- Carry Flag (C)
+					NZVC(0) <= Sum_uns(8);
+			
+			elsif (ALU_Sel = "001") then -- SUBTRACTION
+			
+				-- Sum
+				Sum_uns := unsigned('0' & A) + unsigned('0' & B);
+				Result <= std_logic_vector(Sum_uns(7 downto 0));
+				
+				-- NZVC Flags
+				
+					-- Negative Flag (N)
+					NZVC(3) <= Sum_uns(7); -- Negative Flag
+					
+					-- Zero Flag (Z)
+					if (Sum_uns(7 downto 0) = x"00") then
+						NZVC(2) <= '1';
+					else
+						NZVC(2) <= '0';
+					end if;
+					
+					-- Two's Complement Overflow Flag (V)
+					if ((A(7) = '0' and B(7) = '0' and Sum_uns(7) = '1') or
+						(A(7) = '1' and B(7) = '1' and Sum_uns(7) = '0')) then
+						NZVC(1) <= '1';
+					else
+						NZVC(1) <= '0';
+					end if;
+					
+					-- Carry Flag (C)
+					NZVC(0) <= Sum_uns(8);
+			end if;		
+	end process;
+	
 end architecture;
